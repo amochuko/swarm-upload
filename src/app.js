05444d4a2d344b3c758f90f41474ce9c10d25172
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-05-18 05:22:59 +0100

    fix: check for supplied args

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { parseUrlFlag, fetchAndUploadToSwarm } = require("./utils");
+const { parseUrlFlag, fetchAndUploadToSwarm, isValidURL } = require("./utils");
 
 const args = process.argv.slice(2);
 
@@ -14,25 +14,25 @@ async function main() {
   to a file containing a list of such URLs.
 
   Flags:
-    --file-url                   A single URL or path to a file containing a list of URLs
+    --file-path                   A single URL or path to a file containing a list of URLs
     --filename                   A name given to file
     --bee-node-url              The URL of a Bee node to use
     --stamp-batch-id            The ID of the stamp batch to use on the Bee nod
 
   Usage:
 
-    node app.js --file-url=replace-with-your-file-url --filename=replace-with-your-filename --bee-node-url=replace-with-your-bee-node-url --stamp-batch-id=replace-with-your-bee-node-url
+    node app.js --file-path <replace-with-your-file-url> --filename <replace-with-your-filename> --bee-node-url <replace-with-your-bee-node-url> --stamp-batch-id <replace-with-your-bee-node-url>
 `);
   }
 
-  const urlIndex = args.findIndex((args) => args === "--file-url");
+  const filePathIndex = args.findIndex((args) => args === "--file-path");
   const filenameIndex = args.findIndex((args) => args === "--filename");
   const beeNodeIndex = args.findIndex((args) => args === "--bee-node-url");
   const stampBatchIdIndex = args.findIndex(
     (args) => args === "--stamp-batch-id"
   );
 
-  const filePath = urlIndex !== -1 ? args[urlIndex + 1] : null;
+  const filePath = filePathIndex !== -1 ? args[filePathIndex + 1] : null;
   const fileName = filenameIndex !== -1 ? args[filenameIndex + 1] : null;
   const beeNodeUrl = beeNodeIndex !== -1 ? args[beeNodeIndex + 1] : null;
   const stampBatchId =
@@ -41,16 +41,19 @@ async function main() {
   // Validate and set default values if not provided
   if (!filePath) {
     console.error(
-      "\nError: File path or location is required. Please provide a filePath using --file-path <replace-with-your-file-url>.\n"
+      "\nError: File path or location is required. Please provide a filePath using --file-path <replace-with-your-file-path-or-file-url>.\n"
     );
     process.exit(1);
   }
-  if (!fileName) {
+
+  // check if path is a valid uri; then request for filename
+  if (isValidURL(filePath) && !fileName) {
     console.error(
       "\nError: File name is required. Please provide a filename using --filename <replace-with-your-filename>.\n"
     );
     process.exit(1);
   }
+
   if (!beeNodeUrl) {
     console.error(
       "\nError: Bee node url is required. Please provide a bee-node-url using --bee-node-url <replace-with-your-bee-node-url>.\n"
@@ -67,6 +70,7 @@ async function main() {
 
   try {
     const urls = await parseUrlFlag(filePath, fileName);
+
     await fetchAndUploadToSwarm(urls, beeNodeUrl, stampBatchId);
   } catch (err) {
     console.error(err);
